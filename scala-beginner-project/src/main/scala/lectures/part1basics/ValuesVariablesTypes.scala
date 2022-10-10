3f3139a59150559ddda9cf6182ce4da3b3489699
Plamen <plamkatatihomirov@gmail.com>
2022-10-10 11:44:59 +0300

    Finish with first lesson

---
+++ scala-beginner-project/src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,27 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App{
+
+  //vals are immutable
+  val x: Int = 42
+  println(x)
+
+  //compiler can infer types
+  val y = 43
+
+  //semi-colons are not necessary
+  val aString: String = "Hello";
+  val anotherString = "Hello from me"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4500
+  val aLong: Long = 5600000000067777788L
+  val aFloat: Float = 2.0F
+  val aDouble: Double = 2.13D
+
+  //variables
+  var aVariable: Int = 4
+  aVariable = 5 //side effects
+}
