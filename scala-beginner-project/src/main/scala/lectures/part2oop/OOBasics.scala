96ba2bc844db319c170d49b097bd7c2ba2668190
Plamen <plamkatatihomirov@gmail.com>
2022-10-12 11:21:56 +0300

    OOP lesson finished

--- scala-beginner-project/src/main/scala/lectures/part2oop/OOBasics.scala
+++ scala-beginner-project/src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,10 +1,23 @@
 package lectures.part2oop
 
+import java.time.Year
+
 object OOBasics extends App{
 
   val person = new Person("John", 22)
   println(person.age)
   person.greet("Ivan")
+
+  val author = new Writer("Ivan", "Minchov", 1850)
+  val imposter = new Writer("Ivan", "Minchov", 1850)
+  val novel = new Novel("Under the Yoke", 1888, author)
+
+  println(novel.getAuthorAge())
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter()
+  counter.increase.print
 }
 
 //constructor
@@ -25,3 +38,52 @@ class Person(val name: String,val age: Int = 0) {
 }
 
 //class parameters are not fields
+
+//Novel and a Writer class
+
+class Writer(val firstName: String, val surname: String, val yearOfBirth: Int) {
+  def getFullName(): Unit = {
+    println(s"$firstName $surname")
+  }
+}
+
+class Novel(val name: String, val yearOfRelease: Int, val author: Writer) {
+  def getAuthorAge(): Int = {
+    yearOfRelease - author.yearOfBirth
+  }
+
+  def getAuthor(): Unit = {
+    println(s"The author is: ${author.getFullName()}")
+  }
+
+  def isWrittenBy(author: Writer) = author == this.author
+
+  def copy(year: Int): Novel = {
+    new Novel(name, year, author)
+  }
+}
+
+class Counter(val num: Int = 0) {
+
+  def increase = {
+    println("Incrementing")
+    new Counter(num + 1)
+  }
+
+  def decrease = {
+    println("Decrementing")
+    new Counter(num - 1)
+  }
+
+  def increase(n: Int): Counter = {
+    if(n<=0) this
+    else increase.increase(n-1)
+  }
+
+  def decrease(n: Int): Counter = {
+    if (n <= 0) this
+    else decrease.decrease(n - 1)
+  }
+
+  def print = println(num)
+}
