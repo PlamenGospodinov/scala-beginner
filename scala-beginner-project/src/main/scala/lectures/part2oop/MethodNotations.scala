f8f993ec964992f723adb3909969af27310ed4a6
Plamen <plamkatatihomirov@gmail.com>
2022-10-12 17:52:33 +0300

    Finish with method notations exercise

--- scala-beginner-project/src/main/scala/lectures/part2oop/MethodNotations.scala
+++ scala-beginner-project/src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -4,7 +4,8 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favouriteMovie: String) {
+  class Person(val name: String,val favouriteMovie: String, val age: Int = 0) {
+
     def likes(movie: String): Boolean = movie == favouriteMovie
 
     def hangOutWith(person: Person): String = {
@@ -16,6 +17,18 @@ object MethodNotations extends App {
     def isAlive: Boolean = true
 
     def apply(): String = s"Hi, my name is $name and I like $favouriteMovie"
+
+    def +(nickname: String): Person = {
+      new Person(name+s" $nickname", favouriteMovie)
+    }
+
+    def unary_+ : Person = new Person(name, favouriteMovie, age + 1)
+
+    def learns(language: String) = println(s"$name learns $language")
+
+    def learnsScala() = this learns "Scala"
+
+    def apply(repetitions: Int) = println(s"$name watched her favourite movie $favouriteMovie $repetitions times")
   }
 
   val mary = new Person("Mary", "Inception")
@@ -40,4 +53,23 @@ object MethodNotations extends App {
   //apply
   println(mary.apply())
   println(mary()) // equivalent
+
+  /* 1.Overload the + operator which receives a string and returns a new person with a nickname
+   2.Add an age to the Person class with default 0 value and add unary + operator and
+   returns a new person with the age + 1
+   e.g +mary => mary's age is incremented
+  3.Add a learns method in the Person class =>
+  "Mary learns Scala" for e.g
+  Add a learnsScala method which calls learns with Scala
+  parameter. Use it in postfix notation.
+  4. Overload the apply to receive a number
+  and return "Mary watched her favourite movie ... n times.
+   */
+
+  println(+mary)
+  mary learns "Java"
+  mary.learnsScala()
+  mary(5)
+
+  println((mary + "the bully").name)
 }
