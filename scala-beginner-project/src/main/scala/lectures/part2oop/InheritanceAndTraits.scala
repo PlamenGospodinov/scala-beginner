7fd3b4eb92941c4f6ef1ba807e2a1d3fbf8b8871
Plamen <plamkatatihomirov@gmail.com>
2022-10-12 22:35:37 +0300

    Continue with inheritance

--- scala-beginner-project/src/main/scala/lectures/part2oop/InheritanceAndTraits.scala
+++ scala-beginner-project/src/main/scala/lectures/part2oop/InheritanceAndTraits.scala
@@ -3,6 +3,7 @@ package lectures.part2oop
 object InheritanceAndTraits extends App{
 
   class Animal {
+    val creatureType: String = "Animal"
     private def specific = println("You can't access me")
     protected def eat = println("nomnom")
   }
@@ -23,4 +24,27 @@ object InheritanceAndTraits extends App{
     def this(name: String) = this(name, 0)
   }
   class Adult(name: String, age: Int, idCard: String) extends Person(name)
+
+  //overriding
+  class Dog(override val creatureType: String) extends Animal {
+    override def eat =
+      super.eat
+      println("woof, mnm")
+  }
+  val dog = new Dog("K9")
+  dog.eat
+
+  //type substitution (polymorphism)
+  val dog3: Animal = new Dog("German Shepard")
+  println(dog3.creatureType)
+
+  //super
+
+  //preventing overrides
+  //-final keyword on member
+  //-final keyword on the entire class
+  /*-seal the class = extend classes in this file,
+  prevent extensions in other files - "sealed" keyword
+  */
+
 }
