10ebf3719e29cc4477f5d4435f1b918cb7d4d641
Plamen <plamkatatihomirov@gmail.com>
2022-10-13 08:56:31 +0300

    Finish with inheritance lecture

---
+++ scala-beginner-project/src/main/scala/lectures/part2oop/AbstractDataTypes.scala
@@ -0,0 +1,35 @@
+package lectures.part2oop
+
+object AbstractDataTypes extends App{
+
+  // abstract members
+  abstract class Animal {
+    val creatureType: String
+    def eat: Unit
+  }
+
+  class Dog extends Animal {
+    override val creatureType: String = "Canine"
+
+    override def eat: Unit = println("crunch crunch")
+  }
+
+  //traits
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  trait ColdBlooded
+
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val creatureType: String = "Croc"
+
+    override def eat: Unit = println("Nom Nom Nom")
+
+    override def eat(animal: Animal): Unit = println(s"I am a croc and I am eating ${animal.creatureType}")
+  }
+
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
+}
